e3cb0e4e9fea85a8e0e9c631f614016976dcaf08
Diane42 <diane@42maru.ai>
2024-04-30 10:15:50 +0900

    ✏️ DateRange - calculate date range

--- common/enums/news_enum.py
+++ common/enums/news_enum.py
@@ -1,6 +1,8 @@
 from datetime import datetime, timedelta, time
 from enum import auto
 
+from dateutil.relativedelta import relativedelta
+
 from common.enums import StrEnum
 
 
@@ -27,34 +29,33 @@ class DateRange(StrEnum):
     CUSTOM = auto()
 
     def calculate_date_range(self, start_date=None, end_date=None):
-        #TODO : 테스트용 시간 설정, 개발 완료 후 datetime.now()으로 변경 예정
-        now = datetime(2021, 5, 1, 12, 00, 00)
+        # TODO : 테스트용 시간 설정, 개발 완료 후 datetime.now()으로 변경 예정
+        now = datetime(2021, 5, 1, 12, 12, 12)
 
         if self == DateRange.ALL:
             return None
-        elif self in [DateRange.LAST_1_HOUR, DateRange.LAST_2_HOUR, DateRange.LAST_3_HOUR,
-                      DateRange.LAST_4_HOUR, DateRange.LAST_5_HOUR, DateRange.LAST_6_HOURS]:
-            hours = int(self.name.split('_')[-2])
-            return {"gte": now - timedelta(hours=hours), "lte": now}
-        elif self == DateRange.LAST_1_DAY:
-            return {"gte": now - timedelta(days=1), "lte": now}
-        elif self == DateRange.LAST_1_WEEK:
-            return {"gte": now - timedelta(weeks=1), "lte": now}
-        elif self == DateRange.LAST_1_MONTH:
-            return {"gte": now - timedelta(days=30), "lte": now}
-        elif self == DateRange.LAST_3_MONTHS:
-            return {"gte": now - timedelta(days=90), "lte": now}
-        elif self == DateRange.LAST_6_MONTHS:
-            return {"gte": now - timedelta(days=180), "lte": now}
-        elif self == DateRange.LAST_1_YEAR:
-            return {"gte": now - timedelta(days=365), "lte": now}
+
+        periods = {
+            DateRange.LAST_1_HOUR: timedelta(hours=1),
+            DateRange.LAST_2_HOUR: timedelta(hours=2),
+            DateRange.LAST_3_HOUR: timedelta(hours=3),
+            DateRange.LAST_1_DAY: timedelta(days=1),
+            DateRange.LAST_1_WEEK: timedelta(weeks=1),
+            DateRange.LAST_1_MONTH: relativedelta(months=1),
+            DateRange.LAST_3_MONTHS: relativedelta(months=3),
+            DateRange.LAST_6_MONTHS: relativedelta(months=6),
+            DateRange.LAST_1_YEAR: relativedelta(years=1)
+        }
+        if self in periods:
+            delta = periods[self]
+            return {"gte": (now - delta).isoformat(), "lte": now.isoformat()}
         elif self == DateRange.CUSTOM:
             if start_date and end_date:
-                if isinstance(end_date, datetime):
-                    end_date_with_time = end_date.replace(hour=23, minute=59, second=59)
-                else:
-                    end_date_with_time = datetime.combine(end_date, time(23, 59, 59))
-
-                return {"gte": start_date, "lte": end_date_with_time}
-        else:
-            return None
+                if isinstance(start_date, str):
+                    start_date = datetime.strptime(start_date, "%Y-%m-%d")
+                if isinstance(end_date, str):
+                    end_date = datetime.strptime(end_date, "%Y-%m-%d")
+                start_date_with_time = datetime.combine(start_date, time(0, 0, 0))
+                end_date_with_time = datetime.combine(end_date, time(23, 59, 59))
+                return {"gte": start_date_with_time.isoformat(), "lte": end_date_with_time.isoformat()}
+        return None
