17f4812ff77f556de90f0db8b3011227f5ee39e2
Diane42 <156279294+Diane42@users.noreply.github.com>
2024-05-13 17:10:57 +0900

    :pencil2: delete gunicorn

--- gunicorn.config.py
+++
@@ -1,8 +0,0 @@
-from core.config import settings
-
-bind = f"{settings.API_HOST}:{settings.API_PORT}"
-workers = f"{settings.API_WORKERS}"
-wsgi_app = "core.app:news_app"
-worker_class = "uvicorn.workers.UvicornWorker"
-accesslog = "-"
-timeout = 60
